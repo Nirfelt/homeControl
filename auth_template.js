861b12d4733a767f6e68eb9319299ef953c7e8b2
Sebastian Nirfelt <sebastian.nirfelt@gmail.com>
2017-10-30 00:51:26 +0100

    Added files

---
+++ auth_template.js
@@ -0,0 +1,18 @@
+var auth = {};
+
+//Mongo DB
+auth.mongo_ip = '<ip>';
+auth.mongo_port = '<port>';
+auth.db_name = '<dbname>';
+
+auth.mongo_url = 'mongodb://' + auth.mongo_ip + ':' + auth.mongo_port;
+
+auth.mongo_connection = auth.mongo_url + '/' + auth.db_name;
+
+//Session
+auth.session_secret = '<secret>';
+
+//App
+auth.app_port = '<port>';
+
+module.exports = auth;
