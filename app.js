3ffad6a4222e8f77ef1011f69893fb65678b8178
Sebastian Nirfelt <sebastian.nirfelt@gmail.com>
2017-10-31 19:55:18 +0100

    Added register functionality from guide

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-var auth = require('./auth.js');
+var auth = require('./auth');
 
 mongoose.connect(auth.mongo_connection);
 var db = mongoose.connection;
